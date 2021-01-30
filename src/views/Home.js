f55ac07975f30697d1dd4094dd8df7c320f1b629
rohitbinmile <rohitv@binmile.com>
2021-01-31 00:49:25 +0530

    Code refactoring.

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,46 +1,19 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-
 import CssBaseline from "@material-ui/core/CssBaseline";
 
-import Footer from "../components/dashboard/Footer"
-import NavBar from "../components/dashboard/Header"
-import Sidemenu from "../components/dashboard/Sidemenu/SideMenu"
-
-const drawerWidth = 240;
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: "flex",
-  },
-  appBar: {
-    zIndex: theme.zIndex.drawer + 1,
-  },
-  drawer: {
-    width: drawerWidth,
-    flexShrink: 0,
-  },
-  drawerPaper: {
-    width: drawerWidth,
-  },
-  drawerContainer: {
-    overflow: "auto",
-  },
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing(3),
-  },
-}));
+import Footer from "../components/dashboard/Footer";
+import NavBar from "../components/dashboard/Header";
+import Features from '../components/dashboard/body/features/Features';
 
 export default function ClippedDrawer() {
-  const classes = useStyles();
+  // const classes = useStyles();
 
   return (
-    <div className={classes.root}>
+    <React.Fragment>
       <CssBaseline />
       <NavBar />
-      <Sidemenu />
+      <Features />
       <Footer />
-    </div>
+    </React.Fragment>
   );
 }
