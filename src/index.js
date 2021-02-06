0ed78abcb6901ef79206547ef65b1aa606ff7502
Rkverma1996 <sweetsanjeevkumar96@gmail.com>
2021-02-07 00:26:13 +0530

    Change root reducer path to store.

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import rootReducer from './reducers/rootReducer'
+import rootReducer from './store/home/rootReducer'
 
 const store = createStore(rootReducer)
 
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,88 +1,19 @@
 import React from "react";
-import CssBaseline from "@material-ui/core/CssBaseline";
-// import { makeStyles } from "@material-ui/core/styles";
+import { connect } from 'react-redux';
 
-import Footer from "../components/dashboard/Footer";
-import Grid from "@material-ui/core/Grid";
+import {
+  Grid,
+  CssBaseline,
+} from "@material-ui/core";
+
 import NavBar from "../components/dashboard/Header";
 import Features from "../components/dashboard/body/features/Features";
 import Introduction from "../components/dashboard/body/introduction/Introduction";
 import Description from "../components/dashboard/body/description/Description";
+import Footer from "../components/dashboard/Footer";
 
-// const useStyles = makeStyles((theme) => ({
-//   mainGrid: {
-//     marginTop: theme.spacing(3),
-//   },
-// }));
-
-export default function ClippedDrawer() {
-  // const classes = useStyles();
-
-  const featuredPosts = [
-    {
-      title: "Featured post",
-      date: "Nov 12",
-      description:
-        "This is a wider card with supporting text below as a natural lead-in to additional content.",
-      image: "https://source.unsplash.com/random",
-      imageText: "Image Text",
-    },
-    {
-      title: "Post title",
-      date: "Nov 11",
-      description:
-        "This is a wider card with supporting text below as a natural lead-in to additional content.",
-      image: "https://source.unsplash.com/random",
-      imageText: "Image Text",
-    },
-  ];
-
-  // fake data
-  const post = {
-    title: "Flat 10% Off",
-    image: "url(https://source.unsplash.com/random)",
-    description:
-      "We at Gyanacharya believe that everyone should get education, profit" +
-      "doesn't matter when it comes to value." +
-      "We at Gyanacharya believe that everyone should get education, profit" +
-      "doesn't matter when it comes to value." +
-      "We at Gyanacharya believe that everyone should get education, profit" +
-      "doesn't matter when it comes to value." +
-      "We at Gyanacharya believe that everyone should get education, profit" +
-      "doesn't matter when it comes to value." +
-      "We at Gyanacharya believe that everyone should get education, profit" +
-      "doesn't matter when it comes to value."
-  };
-  const footers = [
-    {
-      title: "Company",
-      description: ["Team", "History", "Contact us", "Locations"],
-    },
-    {
-      title: "Features",
-      description: [
-        "Cool stuff",
-        "Random feature",
-        "Team feature",
-        "Developer stuff",
-        "Another one",
-      ],
-    },
-    {
-      title: "Resources",
-      description: [
-        "Resource",
-        "Resource name",
-        "Another resource",
-        "Final resource",
-      ],
-    },
-    {
-      title: "Legal",
-      description: ["Privacy policy", "Terms of use"],
-    },
-  ];
-
+const ClippedDrawer = (props) => {
+  const { featuredPosts, post, footers } = props;
   return (
     <React.Fragment>
       <CssBaseline />
@@ -98,3 +29,14 @@ export default function ClippedDrawer() {
     </React.Fragment>
   );
 }
+
+const mapStateToProps = (state) => {
+  return {
+    featuredPosts: state.featuredPosts,
+    post: state.post,
+    footers: state.footers,
+
+  }
+}
+
+export default connect(mapStateToProps)(ClippedDrawer);
