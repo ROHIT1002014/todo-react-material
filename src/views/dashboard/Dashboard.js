b43b5838fa3f12c027f96ff7bd5c16b6370d3b25
Rkverma1996 <sweetsanjeevkumar96@gmail.com>
2021-02-07 01:43:49 +0530

    Added more components.

--- src/views/dashboard/Dashboard.js
+++ src/views/dashboard/Dashboard.js
@@ -12,14 +12,22 @@ import {
 import Deposits from "./../../components/home/Deposite";
 import Orders from "./../../components/home/Order";
 import Header from "./../../components/home/header/Header";
+import Marks from "./../../components/home/status/Marks";
+import Profit from "./../../components/home/status/Profit";
+import Progress from "./../../components/home/status/Progress";
+import StudentsStatus from "./../../components/home/status/StudentsStatus";
+import LatestCourse from "./../../components/home/Course/LatestCourse";
+import LatestEnrolledCourse from "./../../components/home/Course/LatestEnrolledCourse";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     display: "flex",
+    paddingBottom: theme.spacing(3),
+    paddingTop: theme.spacing(3),
   },
   appBarSpacer: theme.mixins.toolbar,
   content: {
-    flexGrow: 1,
+    flexGrow: 2,
     height: "100vh",
     overflow: "auto",
   },
@@ -47,13 +55,31 @@ export default function Dashboard() {
       <Header />
       <main className={classes.content}>
         <div className={classes.appBarSpacer} />
-        <Container maxWidth="lg" className={classes.container}>
+        <Container maxWidth="lg">
+          <Grid container spacing={3}>
+            <Grid item lg={3} sm={6} xl={3} xs={12}>
+              <Marks />
+            </Grid>
+            <Grid item lg={3} sm={6} xl={3} xs={12}>
+              <Progress />
+            </Grid>
+            <Grid item lg={3} sm={6} xl={3} xs={12}>
+              <StudentsStatus />
+            </Grid>
+            <Grid item lg={3} sm={6} xl={3} xs={12}>
+              <Profit />
+            </Grid>
+            <Grid item lg={4} md={6} xl={3} xs={12}>
+              <LatestCourse />
+            </Grid>
+            <Grid item lg={8} md={12} xl={9} xs={12}>
+              <LatestEnrolledCourse />
+            </Grid>
+          </Grid>
           <Grid container spacing={3}>
             {/* Chart */}
             <Grid item xs={12} md={8} lg={9}>
-              <Paper className={fixedHeightPaper}>
-                {/* <Chart /> */}
-              </Paper>
+              <Paper className={fixedHeightPaper}></Paper>
             </Grid>
             {/* Recent Deposits */}
             <Grid item xs={12} md={4} lg={3}>
